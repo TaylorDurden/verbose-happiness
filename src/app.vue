ddcd472a32c4fca4726f82de6d2fbf645b72117f
TAYLOR-SKULL\taylorlee <258266864@qq.com>
2018-06-25 18:36:49 +0800

    add

---
+++ src/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="test">{{text}}</div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            text: 'abcdddddd'
+        }
+    }
+}
+</script>
+<style>
+#test{
+    color: red;
+}
+</style>
